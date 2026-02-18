7655f007b84d5fd5160e299bf5dd14ae86b4d0d6
Erick Christian <erickchristian48@gmail.com>
2026-02-19 05:10:37 +0700

    really simplify fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,5 @@
 set -g fish_greeting
+fish_config theme choose gruvbox
 
 # XDG base directories
 set -q XDG_CONFIG_HOME || set -gx XDG_CONFIG_HOME $HOME/.config
@@ -6,76 +7,13 @@ set -q XDG_DATA_HOME || set -gx XDG_DATA_HOME $HOME/.local/share
 set -q XDG_CACHE_HOME || set -gx XDG_CACHE_HOME $HOME/.cache
 set -q XDG_STATE_HOME || set -gx XDG_STATE_HOME $HOME/.local/state
 
-# makefile
-set -gx MAKEFLAGS -j(nproc)
-# ssh agent
-set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+
 # opencode
 set -gx OPENCODE_EXPERIMENTAL true
 set -gx OPENCODE_ENABLE_EXA 1
-# flyctl
-set -gx FLYCTL_INSTALL "$HOME/.fly"
-# XDG_DATA_DIRS - ensure base dirs exist (display managers may already set this)
-if not set -q XDG_DATA_DIRS
-    set -gx XDG_DATA_DIRS /usr/local/share /usr/share
-else
-    # Add base dirs if missing (prepend so DM's additions stay at end)
-    for dir in /usr/share /usr/local/share
-        if not contains $dir $XDG_DATA_DIRS
-            set -gx --prepend XDG_DATA_DIRS $dir
-        end
-    end
-end
-
-# flatpak
-for dir in /var/lib/flatpak/exports/share $HOME/.local/share/flatpak/exports/share
-    if not contains $dir $XDG_DATA_DIRS
-        set -gx --prepend XDG_DATA_DIRS $dir
-    end
-end
 
 # Add tool directories to PATH (highest priority first)
 fish_add_path \
-    $HOME/.local/bin \
-    $HOME/.bun/bin \
-    $XDG_CACHE_HOME/.bun/bin \
-    $HOME/go/bin \
-    $PNPM_HOME \
-    $HOME/.fly/bin \
-    /usr/local/go/bin
-
-# deno
-if test -f "$HOME/.deno/env.fish"
-    source "$HOME/.deno/env.fish"
-end
-
-# cargo
-if test -f "$HOME/.cargo/env.fish"
-    source "$HOME/.cargo/env.fish"
-end
-
-# ASDF
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
-# Load any local overrides or secrets (ignored by git)
-for file in $__fish_config_dir/*local.fish
-    if test -f "$file"
-        source "$file"
-    end
-end
-
-# zoxide: loaded from conf.d/zoxide.fish (cached for faster startup)
-
-# Set gruvbox theme (loads from $__fish_config_dir/themes/)
-fish_config theme choose gruvbox
+    $HOME/.local/bin
