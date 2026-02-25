2c958a1fe8ed86a9e0961b879b95188ec620e292
Erick Christian <erickchristian48@gmail.com>
2026-02-25 22:51:01 +0700

    update again cause i love it

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -40,11 +40,11 @@ in
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      # "https://cache.numtide.com"
+      "https://cache.numtide.com"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      # "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
   };
 
