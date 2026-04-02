6f6d3c268ff8027e2399dcad8d3e0fad91d618dd
Erick Christian <erickchristian48@gmail.com>
2026-04-02 08:40:00 +0700

    wrong file name

--- scripts/gc.sh
+++ scripts/gc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # gc: Garbage collect old Nix generations and rebuild
 
-sudo nix-collect-garbage --delete-old "$@" && ./scripts/rebuild
+sudo nix-collect-garbage --delete-old "$@" && ./scripts/rebuild.sh
