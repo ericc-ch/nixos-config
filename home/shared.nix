d5f713552f3b7d8e9c64d860d14e125a6a8f9fcd
Erick Christian <erickchristian48@gmail.com>
2026-08-19 17:22:19 +0700

    old opencode alias

--- home/shared.nix
+++ home/shared.nix
@@ -183,8 +183,6 @@ in
       shellAliases = {
         c = "clear";
         l = "eza -lah";
-        oc = "opencode attach http://localhost:4096 --dir \"$PWD\"";
-        ocs = "opencode serve --hostname 0.0.0.0 --port 4096 --print-logs 2>&1";
         zed = "zeditor";
       };
       plugins = [
